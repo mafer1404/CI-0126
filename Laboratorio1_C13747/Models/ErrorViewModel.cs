ee55781d40fb1fce18950c4343ceb8578deada4a
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-03-14 15:34:04 -0600

    Creación del proyecto y solución del laboratorio 1

---
+++ Laboratorio1_C13747/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_C13747.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
