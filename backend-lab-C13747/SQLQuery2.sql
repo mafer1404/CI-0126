46b761c1cbd06674dec9c4687907dcd03a3a5263
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-04-12 15:37:46 -0600

    Queries de la base de datos

---
+++ backend-lab-C13747/SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO Pais (Nombre, Idioma, Continente)
+VALUES
+('Costa Rica', 'Español', 'América'),
+('Argentina', 'Español', 'América'),
+('Canada', 'Inglés/Frances', 'América'),
+('Francia', 'Frances', 'Europa'),
+('España', 'Español', 'Europa')
+GO
