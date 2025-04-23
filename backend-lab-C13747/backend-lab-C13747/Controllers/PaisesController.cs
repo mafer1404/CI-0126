e6330a99cf1a5ee2fdf07b4ee3ec1dca52ab724d
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-04-23 00:45:54 -0600

    Laboratorio 5: Método post para guardar los datos de un país nuevo ingresado a la base de datos

--- backend-lab-C13747/backend-lab-C13747/Controllers/PaisesController.cs
+++ backend-lab-C13747/backend-lab-C13747/Controllers/PaisesController.cs
@@ -23,5 +23,25 @@ namespace backend_lab_C13747.Controllers
             return paises;
         }
 
+        [HttpPost]
+        public async Task<ActionResult<bool>> CrearPais(PaisModel pais)
+        {
+            try
+            {
+                if (pais == null)
+                {
+                    return BadRequest();
+                }
+
+                PaisesHandler paisesHandler = new PaisesHandler();
+                var resultado = paisesHandler.CrearPais(pais);
+                return new JsonResult(resultado);
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error creando pais");
+            }
+        }
     }
 }
