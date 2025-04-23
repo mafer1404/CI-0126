1971af959252bafad6885a8c7c34c1a84b1305d2
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-04-23 00:39:54 -0600

    Laboratorio 5: Añadir configuraciones para conectar correctamente front-end con back-end

--- backend-lab-C13747/backend-lab-C13747/Program.cs
+++ backend-lab-C13747/backend-lab-C13747/Program.cs
@@ -1,7 +1,17 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:8080")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,6 +29,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
