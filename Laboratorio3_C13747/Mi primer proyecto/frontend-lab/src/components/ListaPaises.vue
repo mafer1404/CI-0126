c7d574b0b79213d1287db29b232a2bd4f212525a
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-04-23 00:43:52 -0600

    Laboratorio 5: Agregar un mensaje en consola con los datos que se guardaron después de agregar un nuevo país a la tabla

--- Laboratorio3_C13747/Mi primer proyecto/frontend-lab/src/components/ListaPaises.vue
+++ Laboratorio3_C13747/Mi primer proyecto/frontend-lab/src/components/ListaPaises.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="contained mt-5">
-    <h1 class="display-4 text-center">Lista de países</h1>
+    <div class="contained mt-5">
+        <h1 class="display-4 text-center">Lista de países</h1>
         <div class="row justify-content-end">
             <div class="col-2">
                 <a href="/pais">
@@ -11,27 +11,27 @@
             </div>
         </div>
         <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Continente</th>
-          <th>Idioma</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(pais, index) of paises" :key="index">
-          <td>{{  pais.nombre }}</td>
-          <td>{{  pais.continente }}</td>
-          <td>{{  pais.Idioma }}</td>
-          <td>
-            <button class="btn btn-secondary btn-sm">Editar</button>
-            <button class="btn btn-danger btn-sm" v-on:click="eliminarPais(index)">Eliminar</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
- </div>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Continente</th>
+                    <th>Idioma</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(pais, index) of paises" :key="index">
+                    <td>{{  pais.nombre }}</td>
+                    <td>{{  pais.continente }}</td>
+                    <td>{{  pais.idioma }}</td>
+                    <td>
+                        <button class="btn btn-secondary btn-sm">Editar</button>
+                        <button class="btn btn-danger btn-sm" v-on:click="eliminarPais(index)">Eliminar</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
 </template>
 
@@ -54,7 +54,16 @@
       eliminarPais(index) {
         this.paises.splice(index, 1);
       },
-    }
+        obtenerTareas() {
+            axios.get("https://localhost:7110/api/Paises").then((response) => {
+                this.paises = response.data;
+                console.log("Datos a guardar:", this.paises);
+            });
+        },
+    },
+    created: function () {
+        this.obtenerTareas();
+    },
   };
 </script>
 
